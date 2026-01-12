65cf9b6e85010d810b73ac2cd6e50d033533e7e1
마혜준 <70474860+mhjoon99@users.noreply.github.com>
2026-01-12 19:47:43 +0900

    Title: 4가지 관계연산자, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 260112/4가지 관계연산자/four-relational-operators.py
@@ -0,0 +1,25 @@
+a, b = map(int, input().split())
+
+# 1
+if a >= b:
+    print(1)
+else:
+    print(0)
+
+# 2
+if a > b:
+    print(1)
+else:
+    print(0)
+
+# 3
+if a <= b:
+    print(1)
+else:
+    print(0)
+
+# 4
+if a < b:
+    print(1)
+else:
+    print(0)
