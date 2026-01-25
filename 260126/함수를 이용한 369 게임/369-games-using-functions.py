86f866f54e04a4fcce25dc7dd080b990d49c1002
마혜준 <70474860+mhjoon99@users.noreply.github.com>
2026-01-26 01:11:30 +0900

    Title: 함수를 이용한 369 게임, Time: 149ms, Memory: 17MB, Status: Passed - Codetree

--- 260126/함수를 이용한 369 게임/369-games-using-functions.py
+++ 260126/함수를 이용한 369 게임/369-games-using-functions.py
@@ -3,17 +3,10 @@ a, b = map(int, input().split())
 # Please write your code here.
 def find_num(a, b):
     cnt = 0
-    for i in range(a, b+1):
-        _list = map(int, str(i))
-        if 3 in _list:
-            cnt += 1
-        if 6 in _list:
-            cnt += 1
-        if 9 in _list:
-            cnt += 1
-        if i % 3 == 0:
+    for i in range(a, b+1, 1):
+        _list = list(map(int, str(i)))
+        if (3 in _list) or (6 in _list) or (9 in _list) or (i % 3 == 0):
             cnt += 1
     return cnt
 
 print(find_num(a,b))
-    
